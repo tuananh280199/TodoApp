f482078a06975c84143a40c2937bdb22e904e513
tuananh280199 <anhndt@reactplus.com>
2021-12-18 16:58:45 +0700

    first commit

---
+++ src/axios/index.ts
@@ -0,0 +1,44 @@
+// import node_modules
+import axios from 'axios';
+import Config from 'react-native-config';
+
+// main
+
+axios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    config.headers.Authorization = '';
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  },
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    if (error.response?.status === 500) {
+      return Promise.reject(error);
+    }
+    if (error.response?.status === 401) {
+      return Promise.reject(error);
+    }
+    return Promise.reject(error);
+  },
+);
+
+// config axios
+axios.defaults.timeout = 15000;
+axios.defaults.baseURL = Config.API_SERVER;
+axios.defaults.headers.common.Authorization = '';
+
+export default axios;
